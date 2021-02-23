e7a35ca3afb1abeb789c9e6e97f1607898773e28
daisuke furukawa <mogya@mogya.com>
2021-02-23 12:52:02 +0000

    免許svgを表示

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
   */
   modules: [
     '@nuxtjs/pwa',
+    'nuxt-svg-loader',
   ],
   /*
   ** Build configuration
--- pages/license.vue
+++ pages/license.vue
@@ -3,12 +3,18 @@ div
   #header.container.fixed.right-0.left-0.top-0.bg-white.px-1.z-10
     #title-area
       span#title-logo.font-serif.text-3xl.w-8
-        span.italic じょそっこ
+        span.italic じょそっこ免許証
+
+  #main.container.absolute(class="px-1 lg:px-2 xl:px-2")
+    <LisenseSvg />
 </template>
 
 <script>
+import LisenseSvg from '@/assets/images/lisense.svg'
+
 export default {
   components: {
+    LisenseSvg
   },
   data () {
     return {
@@ -24,7 +30,5 @@ export default {
 <style>
 #main{
   top: 120px
-}
-#title-area textarea{
 }
 </style>
