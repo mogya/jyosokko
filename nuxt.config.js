39a0c7b78a5db713278ba32c0a90a8f02bd64be0
daisuke furukawa <mogya@mogya.com>
2021-03-01 11:41:55 +0000

    initialize firebase storage

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/fontawesome.js'
+    '~/plugins/fontawesome.js',
   ],
   /*
   ** Nuxt.js dev-modules
@@ -54,7 +54,24 @@ export default {
   modules: [
     '@nuxtjs/pwa',
     'nuxt-svg-loader',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/firebase',
   ],
+  firebase: {
+    config: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
+    },
+    services: {
+      storage: true
+    }
+  },
   /*
   ** Build configuration
   */
